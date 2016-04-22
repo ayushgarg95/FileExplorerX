32cd853a1807b3fafb1d604a68923ba13b4a639e
Ayush Garg <ayush.nxs@gmail.com>
2016-04-22 10:57:53 +0530

    added delete cut and menu controls

--- try.py
+++ try.py
@@ -3,9 +3,29 @@ import os
 import shutil
 import errno
 
+
 #for running any linux command(Ayudh)
 import subprocess
 from subprocess import call
+#ends here
+
+#dialog box for about
+
+class about(gtk.Dialog):
+
+    def __init__(self, parent):
+        gtk.Dialog.__init__(self, "About ProjectX", parent, 0)
+
+        self.set_default_size(150, 100)
+
+        label = gtk.Label()
+        #label.set_markup("Developed by Avneet Singh Saluja, Ayudh Kumar Gupta, Ayur Jain, Ayush Garg, Ayush Rohatgi")
+        label.set_line_wrap(True)
+
+        box = self.get_content_area()
+        box.add(label) 
+        self.show_all()
+
 #ends here
 
 COL_PATH = 0
@@ -18,30 +38,167 @@ class PyApp(gtk.Window):
     def __init__(self):
         super(PyApp, self).__init__()
 
+        #self.set_icon_from_file("icon.png")  #taskbar icon added
         self.set_size_request(650, 400)
         self.set_position(gtk.WIN_POS_CENTER)
 
         self.connect("destroy", gtk.main_quit)
         self.set_title("ProjectX")
-
-        self.current_directory = '/home/ayush'
+        self.current_directory = '/'
+        
         self.copy_dir = "/home"
         self.paste_dir = "/home"
+        self.copy_dir = "/home/ayush/Downloads/B_large.in"
+        self.copyorcut = 0
+        vbox = gtk.VBox(False, 0);       
+        
+        # Dialog Box
 
-        vbox = gtk.VBox(False, 0);
+        dialog = gtk.MessageDialog(
+        parent         = None,
+        flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
+        type           = gtk.MESSAGE_INFO,
+        buttons        = gtk.BUTTONS_OK,
+        message_format = "Developed by Avneet Singh Saluja, Ayudh Kumar Gupta, Ayur Jain, Ayush Garg, Ayush Rohatgi")
+        dialog.set_title('About ProjectX')
+        dialog.connect('response', lambda dialog, response: dialog.destroy())
+
+        #MenuBar declarations
+        
+        main_menu_bar=gtk.MenuBar()
+       
+        file_menu=gtk.Menu()
+        file_menu_dropdown=gtk.MenuItem("File")
+        file_new=gtk.MenuItem("New Folder")
+        file_open=gtk.MenuItem("Open")
+        file_exit=gtk.MenuItem("Exit")
+        file_menu_dropdown.set_submenu(file_menu)
+        file_menu.append(file_new)
+        file_menu.append(file_open)
+        file_menu.append(file_exit)
+        main_menu_bar.append(file_menu_dropdown)
+        vbox.pack_start(main_menu_bar,False,False,0 )
+
+        file_exit.connect("activate",gtk.main_quit)
+        file_open.connect("activate",self.on_open_clicked,"Open")
+        file_new.connect("activate",self.on_newfolder_clicked)
+
+        edit_menu=gtk.Menu()
+        edit_menu_dropdown=gtk.MenuItem("Edit")
+        edit_cut=gtk.MenuItem("Cut")
+        edit_copy=gtk.MenuItem("Copy")
+        edit_paste=gtk.MenuItem("Paste")
+        edit_delete=gtk.MenuItem("Delete")
+        edit_menu_dropdown.set_submenu(edit_menu)
+        edit_menu.append(edit_cut)
+        edit_menu.append(edit_copy)
+        edit_menu.append(edit_paste)
+        edit_menu.append(edit_delete)
+        main_menu_bar.append(edit_menu_dropdown)
+                
+        edit_cut.connect("activate",self.on_cut,"Cut")
+        edit_copy.connect("activate",self.on_copy,"Copy")
+        edit_paste.connect("activate",self.on_paste,"Paste")
+        edit_delete.connect("activate",self.on_delete,"Delete")
+
+
+        terminal_menu=gtk.Menu()
+        terminal_menu_dropdown=gtk.MenuItem("Terminal")
+        terminal_launchterminal=gtk.MenuItem("Launch Terminal")
+        terminal_launchterminal_pwd=gtk.MenuItem("Launch Terminal in Current Directory")
+        terminal_menu_dropdown.set_submenu(terminal_menu)
+        terminal_menu.append(terminal_launchterminal)
+        terminal_menu.append(terminal_launchterminal_pwd)
+        main_menu_bar.append(terminal_menu_dropdown)
+
+        terminal_launchterminal.connect("activate",self.on_launch_terminal_clicked)
+        terminal_launchterminal_pwd.connect("activate",self.on_launch_terminal_pwd_clicked)
+
+        help_menu=gtk.Menu()
+        help_menu_dropdown=gtk.MenuItem("Help")
+        help_about=gtk.MenuItem("About")
+        help_menu_dropdown.set_submenu(help_menu)
+        help_menu.append(help_about)
+        main_menu_bar.append(help_menu_dropdown)
+
+        #help_about.connect("activate",self.on_about_clickedtp)
+        help_about.connect("activate", lambda *args: dialog.show())
+    
+        #ends here
 
         #Toolbars for bifurcation
+       
         title_toolbar=gtk.Toolbar()
         vbox.pack_start(title_toolbar, False, False, 0)
 
-        menu_toolbar=gtk.Toolbar()
-        vbox.pack_start(menu_toolbar, False, False, 0)
+        bookmarks_toolbar=gtk.Toolbar()
+        vbox.pack_start(bookmarks_toolbar, False, False, 0)
+         
+        infobar_toolbar=gtk.Toolbar()
+        vbox.pack_end(infobar_toolbar,False,False,0)
          
 
         #ends here
 
+        #current directory bar
+ 
+        self.pwd=gtk.Label()
+        self.pwd.set_markup(self.current_directory)
+
+        item3 = gtk.ToolItem()
+        item3.add(self.pwd)
+        infobar_toolbar.insert(item3, -1)
+
+        #ends here
+
+
+        #bookmarks bar
+     
+        bookmarks_label = gtk.Label("Go To :")
+        item1 = gtk.ToolItem()
+        item1.add(bookmarks_label)
+        bookmarks_toolbar.insert(item1, -1)
+
+        sep1 = gtk.SeparatorToolItem()  #seperator
+        bookmarks_toolbar.insert(sep1, 1)
+
+
+        self.desktop_Button = gtk.ToolButton(label="Desktop")
+        self.desktop_Button.set_is_important(True)
+        bookmarks_toolbar.insert(self.desktop_Button, -1)
+
+        self.documents_Button = gtk.ToolButton(label="Documents")
+        self.documents_Button.set_is_important(True)
+        bookmarks_toolbar.insert(self.documents_Button, -1)
+
+        self.downloads_Button = gtk.ToolButton(label="Downloads")
+        self.downloads_Button.set_is_important(True)
+        bookmarks_toolbar.insert(self.downloads_Button, -1)
+
+        self.music_Button = gtk.ToolButton(label="Music")
+        self.music_Button.set_is_important(True)
+        bookmarks_toolbar.insert(self.music_Button, -1)
+
+        self.pictures_Button = gtk.ToolButton(label="Pictures")
+        self.pictures_Button.set_is_important(True)
+        bookmarks_toolbar.insert(self.pictures_Button, -1)
+
+        self.videos_Button = gtk.ToolButton(label="Videos")
+        self.videos_Button.set_is_important(True)
+        bookmarks_toolbar.insert(self.videos_Button, -1)
+        
+        #ends here
+
+        #up home forward back search toolbar declaration
+        
         toolbar = gtk.Toolbar()
+
         vbox.pack_start(toolbar, False, False, 0)
+
+       
+        #ends here
+
+        #home and up buttons
 
         self.upButton = gtk.ToolButton(gtk.STOCK_GO_UP);
         self.upButton.set_is_important(True)
@@ -52,7 +209,10 @@ class PyApp(gtk.Window):
         homeButton.set_is_important(True)
         toolbar.insert(homeButton, -1)
 
+        #ends here
+
         # Back and Forward buttons
+       
         self.backButton = gtk.ToolButton(gtk.STOCK_GO_BACK)
         self.backButton.set_is_important(True)
         self.backButton.set_sensitive(False)
@@ -63,25 +223,22 @@ class PyApp(gtk.Window):
         self.forwardButton.set_sensitive(False)
         toolbar.insert(self.forwardButton, -1)
 
-
         #ends here
 
+        #search bar
+        sep = gtk.SeparatorToolItem() #seperator
+        toolbar.insert(sep, 4)
 
-
-        #search bar(Ayudh)
-        
         self.searchfile=gtk.Entry()
         self.searchfile.set_text("Search")
         item = gtk.ToolItem()
         item.add(self.searchfile)
         toolbar.insert(item, -1)
         
-
-        searchButton = gtk.ToolButton(gtk.STOCK_FIND)
-        searchButton.set_is_important(True)
-        toolbar.insert(searchButton, -1)
-
-
+        self.searchButton = gtk.ToolButton(gtk.STOCK_FIND)
+        self.searchButton.set_is_important(True)
+        toolbar.insert(self.searchButton, -1)
+        self.searchButton.connect("clicked",self.searchfunc)
 
         #ends here
 
@@ -102,10 +259,28 @@ class PyApp(gtk.Window):
         iconView = gtk.IconView(self.store)
         iconView.set_selection_mode(gtk.SELECTION_MULTIPLE)
 
+
+        #bookmarks toolbar signal connection
+       
+        self.desktop_Button.connect("clicked", self.on_desktop_clicked)
+        self.documents_Button.connect("clicked", self.on_documents_clicked)
+        self.downloads_Button.connect("clicked", self.on_downloads_clicked)
+        self.music_Button.connect("clicked", self.on_music_clicked)
+        self.pictures_Button.connect("clicked", self.on_pictures_clicked)
+        self.videos_Button.connect("clicked", self.on_videos_clicked)
+        
+        #ends here
+
+        #signal connection of up,home,forward and back buttons
+
         self.upButton.connect("clicked", self.on_up_clicked)
         homeButton.connect("clicked", self.on_home_clicked)
         self.backButton.connect("clicked", self.on_back_clicked)
         self.forwardButton.connect("clicked", self.on_forward_clicked)
+
+        #ends
+
+       
 
         iconView.set_text_column(COL_PATH)
         iconView.set_pixbuf_column(COL_PIXBUF)
@@ -113,6 +288,7 @@ class PyApp(gtk.Window):
         iconView.connect("item-activated", self.on_item_activated)  #extra
 
         iconView.connect("selection-changed", self.on_item_clicked)   #extra
+        
         # extra 
         eventbox.add(iconView)
 
@@ -127,16 +303,45 @@ class PyApp(gtk.Window):
         item2 = gtk.MenuItem("Copy")
         rightClickMenu.append(item2)    
         item2.connect("activate",self.on_copy,"Copy")
-
-        item3 = gtk.MenuItem("Paste")
-        rightClickMenu.append(item3)
-        item3.connect("activate",self.on_paste,"Paste")
-
         
+        item3 = gtk.MenuItem("Cut")
+        rightClickMenu.append(item3)
+        item3.connect("activate",self.on_cut,"Cut")
+
+        item4 = gtk.MenuItem("Paste")
+        rightClickMenu.append(item4)
+        item4.connect("activate",self.on_paste,"Paste")
+
+        item5 = gtk.MenuItem("Delete")
+        rightClickMenu.append(item5)
+        item5.connect("activate",self.on_delete,"Delete")
+
+        # Terminal submenu
+        terminalMenu = gtk.Menu()
+
+        item6 = gtk.MenuItem("Launch Terminal")
+        terminalMenu.append(item6)
+        item6.connect("activate",self.on_launch_terminal_clicked)
+
+        item7 = gtk.MenuItem("Launch Terminal in Current Directory")
+        terminalMenu.append(item7)
+        item7.connect("activate",self.on_launch_terminal_pwd_clicked)
+        
+        item8 = gtk.MenuItem("Terminal")
+        rightClickMenu.append(item8)
+
+        item8.set_submenu(terminalMenu)
+
         item1.show()
         item2.show()
         item3.show()
+        item4.show()
+        item5.show()
+        item6.show()
+        item7.show()
+        item8.show()
 
+        terminalMenu.show()
         rightClickMenu.show()
         
         eventbox.connect_object("button-press-event", self.on_button_press_event,rightClickMenu)
@@ -145,8 +350,6 @@ class PyApp(gtk.Window):
         
         # RIght Click ends
 
-
-       
         sw.add_with_viewport(eventbox)
 
         # extra ends
@@ -171,6 +374,9 @@ class PyApp(gtk.Window):
 
         if self.current_directory == None:
             return
+        
+        self.pwd.set_markup(self.current_directory)
+        
 
         for fl in os.listdir(self.current_directory):
 
@@ -179,15 +385,19 @@ class PyApp(gtk.Window):
                     self.store.append([fl, self.dirIcon, True])
                 else:
                     self.store.append([fl, self.fileIcon, False])
+                         
 
-
+    #home button function
 
     def on_home_clicked(self, widget):
         self.current_directory = os.path.realpath(os.path.expanduser('~'))
         self.fill_store()
         self.upButton.set_sensitive(True)
 
-# back and fwd functions
+    #ends here
+
+    # back and fwd functions 
+
     def on_back_clicked(self, widget):
         
         forward_stack.append(self.current_directory)
@@ -207,7 +417,8 @@ class PyApp(gtk.Window):
         self.backButton.set_sensitive(True)
         self.fill_store()
         self.upButton.set_sensitive(True)         
-#end here
+        
+        #end here
     
 
     def on_item_clicked(self, widget):
@@ -217,7 +428,6 @@ class PyApp(gtk.Window):
             return
         item = mm[0]
 
-        print "SELECTION CHANGED"
         global model1 
         model1 = widget.get_model()
         
@@ -226,6 +436,8 @@ class PyApp(gtk.Window):
         
         global isDir1 
         isDir1 = model1[item][COL_IS_DIRECTORY]
+
+        
 
     def on_open_clicked(self,widget,item):
         if not isDir1:
@@ -251,7 +463,7 @@ class PyApp(gtk.Window):
 
         # opens a file withs its default preferred application(Ayudh)
         if not isDir:
-            subprocess.call(["xdg-open",self.current_directory +"/"+path])  
+            subprocess.call(["xdg-open",self.current_directory +"/"+path])
             return
         #ends here   
  
@@ -264,6 +476,8 @@ class PyApp(gtk.Window):
         self.fill_store()
         self.upButton.set_sensitive(True)
         #ends
+    
+    #up button function
 
     def on_up_clicked(self, widget):
         self.current_directory = os.path.dirname(self.current_directory)
@@ -272,7 +486,10 @@ class PyApp(gtk.Window):
         if self.current_directory == "/": sensitive = False
         self.upButton.set_sensitive(sensitive)
 
+    #ends here
+
     # Right Click Event
+
     def on_button_press_event(self, widget, event):
             # Check if right mouse button was preseed
             if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
@@ -303,17 +520,114 @@ class PyApp(gtk.Window):
 
     def on_copy(self,widget,event):
         self.copy_dir = self.current_directory + os.path.sep + path1
+        self.copyorcut = 0
         global path2
         path2 = path1
         print path2
         print self.copy_dir
 
+    def on_cut(self,widget,event):
+        self.copy_dir = self.current_directory + os.path.sep + path1
+        self.copyorcut = 1
+        global path2
+        path2 = path1
+        print path2
+        print self.copy_dir
+
+
     def on_paste(self,widget,event):
         self.paste_dir = self.current_directory + os.path.sep + path2
         print self.copy_dir
         print self.paste_dir
-        self.copy(self.copy_dir,self.paste_dir)
-        print "Copied"
+        if self.copyorcut == 0:
+            self.copy(self.copy_dir,self.paste_dir)
+            print "Copied"
+        else:
+            shutil.move(self.copy_dir,self.paste_dir)
         self.fill_store()
+
+    def delete(self, path):
+        try:
+            shutil.rmtree(path)
+        except OSError as e:
+            # If the error was caused because the source wasn't a directory
+            if e.errno == errno.ENOTDIR:
+                os.remove(path)
+            else:
+                print('Directory not copied. Error: %s' % e)
+
+    def on_delete(self, widget, event):
+        self.delpath = self.current_directory + os.path.sep + path1
+        self.delete(self.delpath)
+        print self.delpath, "- deleted"
+        self.fill_store()
+
+    #Bookmarks bar functions
+
+    def on_desktop_clicked(self,widget):
+        self.current_directory = os.path.realpath(os.path.expanduser('~/Desktop'))
+        self.fill_store()
+        self.upButton.set_sensitive(True)
+
+    def on_documents_clicked(self,widget):
+        self.current_directory = os.path.realpath(os.path.expanduser('~/Documents'))
+        self.fill_store()
+        self.upButton.set_sensitive(True)  
+        
+    def on_downloads_clicked(self,widget):
+        self.current_directory = os.path.realpath(os.path.expanduser('~/Downloads'))
+        self.fill_store()
+        self.upButton.set_sensitive(True)  
+        
+    def on_music_clicked(self,widget):
+        self.current_directory = os.path.realpath(os.path.expanduser('~/Music'))
+        self.fill_store()
+        self.upButton.set_sensitive(True)  
+
+
+    def on_pictures_clicked(self,widget):
+        self.current_directory = os.path.realpath(os.path.expanduser('~/Pictures'))
+        self.fill_store()
+        self.upButton.set_sensitive(True)  
+        
+    def on_videos_clicked(self,widget):
+        self.current_directory = os.path.realpath(os.path.expanduser('~/Videos'))
+        self.fill_store()
+        self.upButton.set_sensitive(True)         
+
+    #ends here
+
+
+    #search function
+    
+    def searchfunc(self,widget):
+        search_query=self.searchfile.get_text()
+        print search_query
+    
+    #ends here
+
+    #terminal menu commands
+    
+    def on_launch_terminal_clicked(self,widget):
+      temp2="--working-directory="
+      temp3="/~"
+      subprocess.call(["gnome-terminal",temp2+temp3])
+
+    def on_launch_terminal_pwd_clicked(self,widget):
+      temp1="--working-directory="
+      subprocess.call(["gnome-terminal",temp1+self.current_directory])    
+
+    #ends here
+
+    #about dialog box func
+
+    #def on_about_clicked(self, widget):
+        #dialog = about(self)
+     
+    #ends here
+    def on_newfolder_clicked(self,widget):
+        new_folder_path = self.current_directory + os.path.sep + "New_folder"
+        os.mkdir(new_folder_path,0755)
+
 PyApp()
 gtk.main()
